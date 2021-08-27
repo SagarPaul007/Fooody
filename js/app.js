7ec99b2422d5c26e1be824b142bcad4c47e44138
Sagar Paul <48364544+SagarPaul007@users.noreply.github.com>
2021-08-27 08:02:56 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,68 @@
-let btn = document.querySelector(".btn");
+let btnTop = document.querySelector(".btn-top");
+let btnBottom = document.querySelector(".btn-bottom");
+let searchTop = document.querySelector(".search-top");
+let searchBottom = document.querySelector(".search-bottom");
+let result = document.querySelector(".result");
+
+
+// functions
+
+function showRecipe(data, s) {
+    let meals = data.meals;
+    if (!meals) {
+        alert(`Ooops... No Results found for "${s}", You may have misspelled the dish name or Our database dosen't have the recipe for this dish rn!`);
+        return;
+    }
+
+    document.querySelector(".random-recipe").innerHTML = `Search Result for "${s}"`;
+    document.getElementById("random").scrollIntoView();
+
+    let x = "";
+
+    meals.forEach(meal => {
+
+        let {
+            strMeal,
+            strArea,
+            strInstructions
+        } = meal;
+
+        let ingredients = [];
+        let arr;
+
+        for (let i = 1; i <= 20; i++) {
+            if (meal[`strIngredient${i}`]) ingredients.push(`${meal[`strIngredient${i}`]} : ${meal[`strMeasure${i}`]}`);
+        }
+        for (let i = 0; i < 20; i++) {
+            arr = ingredients.map(ing => `<div class="item">${ing}</div>`);
+        }
+        arr.forEach(item => {
+            document.querySelector(".ingridients").innerHTML += item;
+        });
+
+        x += `<div class="recipe">
+                    <div class="introduction">
+                        <h3 class="name">${strMeal}</h3>
+                        <p class="area">${strArea}</p>
+                    </div>
+
+                    <div class="ingridients">${arr.join(" ")}</div>
+
+                    <div class="instruction">
+                        <p>${strInstructions}</p>
+                    </div>
+                </div>`;
+    });
+
+    result.innerHTML = x;
+
+}
+
+// events
 
 window.addEventListener("DOMContentLoaded", () => {
     fetch(`https://www.themealdb.com/api/json/v1/1/random.php`).then(res => res.json()).then(data => {
         let meal = data.meals[0];
-        console.log(meal);
-
         let {
             strMeal,
             strArea,
@@ -30,6 +88,13 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-btn.addEventListener("click", () => {
-    console.log("hello");
+btnTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    let s = searchTop.value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${s}`).then(res => res.json()).then(data => showRecipe(data, s));
+});
+btnBottom.addEventListener("click", (e) => {
+    e.preventDefault();
+    let s = searchBottom.value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${s}`).then(res => res.json()).then(data => showRecipe(data, s));
 });
